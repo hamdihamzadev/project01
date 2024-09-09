f7b70987a915a1ab061248cc4b3bfd862ad924c2
hamdi hamza <hamdihamzadev@hotmail.com>
2024-09-09 11:52:04 +0100

    myProV1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'? '/FrontDeployVercel/': '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/': '/',
   devServer: {
     historyApiFallback: true  // This ensures the app serves index.html for any route
   }
